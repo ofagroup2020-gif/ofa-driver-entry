3707912dd1808e50dde5b60ab687679587c01110
ofagroup2020-gif <ofa.group2020@gmail.com>
2026-02-02 15:18:07 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-// 最低限のSW（表示を安定させる）
-self.addEventListener("install", (e)=> self.skipWaiting());
-self.addEventListener("activate", (e)=> self.clients.claim());
+self.addEventListener("fetch", () => {});
