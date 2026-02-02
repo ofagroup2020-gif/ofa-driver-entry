e26b504c78cf9d3f2d19b9d4a04787bd6601e3dd
ofagroup2020-gif <ofa.group2020@gmail.com>
2026-02-02 16:13:23 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
-self.addEventListener("fetch", () => {});
+const CACHE_NAME = "ofa-entry-v20260202-1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css?v=20260202-1",
+  "./app.js?v=20260202-1",
+  "./manifest.webmanifest",
+  "./assets/logo.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const keys = await caches.keys();
+      await Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)));
+      await self.clients.claim();
+    })()
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req))
+  );
+});
